d045452b4fba4dc9798466b1af6638991adcea61
FaRnaM-Hs <farnam.hs@yahoo.com>
2022-08-04 19:19:50 +0430

    Primitive Function Should Return A Primitive Type Given Any Type

--- src/test/java/functional/interfaces/PrimitiveFunctionShould.java
+++ src/test/java/functional/interfaces/PrimitiveFunctionShould.java
@@ -3,9 +3,7 @@ package functional.interfaces;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.function.DoubleFunction;
-import java.util.function.IntFunction;
-import java.util.function.LongFunction;
+import java.util.function.*;
 
 public class PrimitiveFunctionShould {
 
@@ -22,5 +20,15 @@ public class PrimitiveFunctionShould {
         LongFunction<String> toStringLong = String::valueOf;
         String result3 = toStringLong.apply(2022L);
         Assertions.assertThat(result3).isEqualTo("2022");
+    }
+
+    @Test
+    void return_a_primitive_type_given_any_type() {
+        ToIntFunction<String> toIntFunction = string -> Integer.valueOf(string).intValue();
+        int stringToInt = toIntFunction.applyAsInt("5");
+        Assertions.assertThat(stringToInt).isEqualTo(5);
+
+        // ToDoubleFunction<Integer> toDoubleFunction;
+        // ToLongFunction<Double> toLongFunction;
     }
 }
