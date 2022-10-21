7780b80ce0a8c1ef995fe82d97925251a0e6f55c
FaRnaM-Hs <farnam.hs@yahoo.com>
2022-10-21 16:42:05 +0330

    Benefits of Optional

--- src/test/java/optional/OptionalShould.java
+++ src/test/java/optional/OptionalShould.java
@@ -3,25 +3,31 @@ package optional;
 import football.team.Coach;
 import football.team.Degree;
 import football.team.Team;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class OptionalShould {
 
     @Test
     void avoid_null_problems() {
+        Team team = new Team(new Coach(new Degree("A")));
 
-        // Before Java 8
-        Team team = new Team();
-        if (team != null) {
-            Coach coach = team.getCoach();
-            if (coach != null) {
-                Degree degree = coach.getDegree();
-                if (degree != null) {
-                    String value = degree.getValue();
-
-                    System.out.println(value);
-                }
+        // Before java 8
+/*        if (team != null) {
+            final Optional<Coach> coach = team.getCoach();
+            if (coach.isPresent()) {
+                final Optional<Degree> degree = coach.get().getDegree();
+                final String value = degree.get().getValue();
+                assertThat(value).isEqualTo(null);
             }
-        }
+        }*/
+
+        // From java 8
+        team.getCoach()
+                .flatMap(Coach::getDegree)
+                .map(Degree::getValue)
+                .ifPresent(value -> assertThat(value).isEqualTo("A"));
     }
 }
